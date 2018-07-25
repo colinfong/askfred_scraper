64a65f96d64180b471be19f9420af6bc9c1016f9
tonycheang <cheangytony@gmail.com>
2018-07-25 00:54:39 -0700

    made use of index_of_csv_div variable for clarity

--- scraping_tournament_csv_link.py
+++ scraping_tournament_csv_link.py
@@ -4,9 +4,10 @@ from bs4 import BeautifulSoup
 page = requests.get("https://askfred.net/Results/results.php?tournament_id=39072")
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# get side bar with all links including csv download
 results_head = list(soup.find_all('div', id='other-links')[0])
-index_of_csv_div = 9
-a_tag = list(results_head[7].children)[0]
+index_of_csv_div = 7
+a_tag = list(results_head[index_of_csv_div].children)[0]
 href = a_tag.get('href')
 
 print(href)
