76ee704dc9446840b71cc8888eb9ce68853bf2b0
colinfong <colinjoefong@gmail.com>
2018-07-25 22:24:22 -0700

    got all CSVs

---
+++ download_csvs.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import os
+import errno
+import re
+
+with open("./output/all_csv_links.json") as f:
+    data = json.load(f)
+
+for i,csv_link in enumerate(data):
+    
+    matches = re.findall(".*_id=([0-9]*)", csv_link)
+    tournament_id = matches[0]
+    print("Downloading " + str(tournament_id) + " from " + csv_link)
+    print(str(i) + " of " + str(len(data)))
+    filename = "./tourney_csvs/" + str(tournament_id)
+    if not os.path.exists(os.path.dirname(filename)):
+        try:
+            os.makedirs(os.path.dirname(filename))
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+    
+    r = requests.get(csv_link, allow_redirects=True)
+    with open(filename, "w") as f:
+        f.write(r.content)
