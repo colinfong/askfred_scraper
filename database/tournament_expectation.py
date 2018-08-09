875b916cf9d6b4992109869b237042d954e5894f
colinfong <colinjoefong@gmail.com>
2018-08-08 20:11:30 -0700

    Changed stat column names in tournament_results

--- database/tournament_expectation.py
+++ database/tournament_expectation.py
@@ -16,9 +16,9 @@ except:
     print("Error connecting.")
 
 
-# Add new columns to instances
-db.query("""ALTER TABLE instances ADD expected_placing VARCHAR(6)""")
-db.query("""ALTER TABLE instances ADD placing_outcome INT""")
+# Add new columns to tournament_results
+db.query("""ALTER TABLE tournament_results ADD expected_rating_for_placement VARCHAR(6)""")
+db.query("""ALTER TABLE tournament_results ADD weighted_performance INT""")
 
 cursor = db.cursor()
 
@@ -28,42 +28,42 @@ data = cursor.fetchall()
 
 first = True
 for t in data:
-    # Get tournament result ids
-    cursor.execute("SELECT id, rating_before FROM instances WHERE tournament_id ='" + t[0] + "'")
+    # Get all results of a single tournament
+    cursor.execute("SELECT id, rating_before FROM tournament_results WHERE tournament_id =" + str(t[0]) + "")
     tournament_results = cursor.fetchall()
     result_ids = []
     for result in tournament_results:
         rating = ""
         if result[1] != "":
+            # First character of rating EX: A2012
             rating = result[1][0]
+        # Result ID and rating before of ID
         result_ids.append([result[0], rating])
 
     # Get ratings in order
-    cursor.execute("SELECT rating_before FROM instances WHERE tournament_id = '" + t[0] + "' ORDER BY rating_before")
+    cursor.execute("SELECT rating_before FROM tournament_results WHERE tournament_id = " + str(t[0]) + " ORDER BY rating_before")
     ordered_ratings = cursor.fetchall()
     sorted_ratings = []
     for rating in ordered_ratings:
         # Some tournaments have empty rating_before fields
         if rating[0] != "":
             sorted_ratings.append(rating[0][0])
-    # Insert expected_placing and placing_outcome
+    # Insert expected_rating_for_placement and weighted_performance
     if len(sorted_ratings) > 0:
         for place in range(len(result_ids)):
             if result_ids[place][1] != "":
                 actual = getattr(ratings, result_ids[place][1]).value
                 expected = getattr(ratings, sorted_ratings[place]).value
-                expected_placing = "expected_placing='" + sorted_ratings[place] + "'"
-                # print(expected_placing)
-                placing_outcome = "placing_outcome = '" + str(expected-actual) + "'"
+                expected_rating_for_placement = "expected_rating_for_placement='" + sorted_ratings[place] + "'"
+                # print(expected_rating_for_placement)
+                weighted_performance = "weighted_performance = '" + str(expected-actual) + "'"
                 result_id = "'" + str(result_ids[place][0]) + "'"
                 # print(result_id)
-                cursor.execute("UPDATE instances SET " + expected_placing + "," + placing_outcome + "WHERE id=" + result_id)
+                cursor.execute("UPDATE tournament_results SET " + expected_rating_for_placement + "," + weighted_performance + "WHERE id=" + result_id)
     
 cursor.close()
 db.commit()
 db.close()
 
-    # if first = True:
-    #     data = cursor.fetchall()
 
 
