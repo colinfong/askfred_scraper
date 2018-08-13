c29f9c06647e108d4656e94a2bf6971e461ca099
tonycheang <cheangytony@gmail.com>
2018-08-12 20:49:35 -0700

    integrated tournament_expectations.py into new class structure

--- database/add_stats_table.py
+++ database/add_stats_table.py
@@ -23,7 +23,8 @@ class Tournament:
             print("Error connecting.")
 
         self.number_of_participants = self._calculate_number_of_participants()
-        self.ratings = self._retrieve_ratings()
+        self.result_ids = self._retrieve_result_ids()
+        self.sorted_ratings = self._calculate_sorted_ratings()
 
         cursor.close()
         db.commit()
@@ -36,7 +37,7 @@ class Tournament:
         # print("Participants: " + str(number_of_participants) + ".")
         return number_of_participants
 
-    def _retrieve_ratings(self):
+    def _retrieve_result_ids(self):
         # Get all results of a single tournament
         cursor.execute("SELECT id, rating_before FROM tournament_results WHERE tournament_id =" + str(self.id_num) + "")
         tournament_results = cursor.fetchall()
@@ -49,14 +50,26 @@ class Tournament:
             # Result ID and rating before of ID
             result_ids.append([result[0], rating])
         return result_ids
+    
+    def _calculate_sorted_ratings(self):
+        # Get ratings in order
+        cursor.execute("SELECT rating_before FROM tournament_results WHERE tournament_id = " + str(self.id_num) + " ORDER BY rating_before")
+        ordered_ratings = cursor.fetchall()
+        sorted_ratings = []
+        for rating in ordered_ratings:
+            # Some tournaments have empty rating_before fields
+            if rating[0] != "":
+                sorted_ratings.append(rating[0][0])
+        return sorted_ratings
 
 # class to build a single row, helps organize methods
 class Stats_Row(Tournament):
-    def __init__(self, tournament, tournament_results_id, tournament_id, last_name, first_name):
+    def __init__(self, tournament, tournament_results_id, tournament_id, place, last_name, first_name):
 
         self.tournament = tournament
         self.tournament_results_id = tournament_results_id
         self.tournament_id = tournament_id
+        self.place = place
         self.last_name = last_name
         self.first_name = first_name
 
@@ -71,9 +84,10 @@ class Stats_Row(Tournament):
         # calculations
         try: 
             self.inverted_placement = self._calculate_inverted_placement()
-            #self.expected_rating_for_placement = self._calculate_expected_rating_for_placement()
-            #self.weighted_performance = self._calculate_weighted_performance()
-            #self.performance_points = self._calculate_performance_points()
+            self.expected_rating_for_placement = self._calculate_expected_rating_for_placement()
+            self.weighted_performance = self._calculate_weighted_performance()
+            self.performance_points = self._calculate_performance_points()
+            # median expected placement?
             #self.expected_placement = self._calculate_expected_placement()
             #self.expected_performance_points = self._calculate_expected_performance_points()
         except:
@@ -86,7 +100,7 @@ class Stats_Row(Tournament):
 
     # builds a query to insert row
     def get_insert_query(self):
-        query = "inverted_placement: " + str(self.inverted_placement)
+        query = "first_name: " + self.first_name + " expected_rating_for_placement: " + self.expected_rating_for_placement + " weighted_performance: " + str(self.weighted_performance)
         return query
 
     # following methods used to performance row calculations during __init__
@@ -100,7 +114,7 @@ class Stats_Row(Tournament):
         return self.tournament.number_of_participants - placement + 1
 
     def _calculate_performance_points(self):
-        return None
+        return self.weighted_performance * self.inverted_placement
 
     def _calculate_expected_performance_points(self):
         return None
@@ -109,32 +123,19 @@ class Stats_Row(Tournament):
         return None
 
     def _calculate_weighted_performance(self):
-        return None
+        if self.tournament.number_of_participants > 0:
+            if self.tournament.result_ids[self.place - 1][1] != "":
+                actual = getattr(ratings, self.tournament.result_ids[self.place - 1][1]).value
+                expected = getattr(ratings, self.tournament.sorted_ratings[self.place - 1]).value
+        # 1 to remove push up 0 weights (what about -1 weights? does that make sense)
+        return expected - actual + 1
 
     def _calculate_expected_rating_for_placement(self):
-        '''
-        # Get ratings in order
-        cursor.execute("SELECT rating_before FROM tournament_results WHERE tournament_id = " + str(self.tournament_id) + " ORDER BY rating_before")
-        ordered_ratings = cursor.fetchall()
-        sorted_ratings = []
-        for rating in ordered_ratings:
-            # Some tournaments have empty rating_before fields
-            if rating[0] != "":
-                sorted_ratings.append(rating[0][0])
         # Insert expected_rating_for_placement and weighted_performance
-        if len(sorted_ratings) > 0:
-            for place in range(len(result_ids)):
-                if result_ids[place][1] != "":
-                    actual = getattr(ratings, result_ids[place][1]).value
-                    expected = getattr(ratings, sorted_ratings[place]).value
-                    expected_rating_for_placement = "expected_rating_for_placement='" + sorted_ratings[place] + "'"
-                    # print(expected_rating_for_placement)
-                    weighted_performance = "weighted_performance = '" + str(expected-actual) + "'"
-                    result_id = "'" + str(result_ids[place][0]) + "'"
-                    # print(result_id)
-                    cursor.execute("UPDATE tournament_results SET " + expected_rating_for_placement + "," + weighted_performance + "WHERE id=" + result_id)
-        '''
-        return None
+        if self.tournament.number_of_participants > 0:
+            if self.tournament.result_ids[self.place - 1][1] != "":
+                expected_rating_for_placement = self.tournament.sorted_ratings[self.place - 1]
+        return expected_rating_for_placement
 
 
 
@@ -151,7 +152,6 @@ def add_tournament_stats(tournament_id, db_cursor):
     tournament_results = db_cursor.fetchall()
 
     this_tournament = Tournament(tournament_id)
-    print(this_tournament.ratings)
 
     print("\n NEW TOURNAMENT \n")
     # builds and inserts a stats row corresponding to each entry in tournament results
@@ -159,20 +159,16 @@ def add_tournament_stats(tournament_id, db_cursor):
 
         tournament_results_id = row[0]
         tournament_id = row[1]
+        place = row[2]
         last_name = row[3]
         first_name = row[4]
         # print(row)
 
         try:
-            current_row = Stats_Row(this_tournament, tournament_results_id, tournament_id, last_name, first_name)
+            current_row = Stats_Row(this_tournament, tournament_results_id, tournament_id, place, last_name, first_name)
             print(current_row.get_insert_query())
         except:
             print("Failed to build row where tournament_results(id) = " + str(tournament_results_id))
-
-
-
-
-
 
 ########### Program to add table ###########
 
